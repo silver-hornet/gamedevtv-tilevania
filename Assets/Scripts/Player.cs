03e883e97e0be7fb352518eec04e1ecef23df298
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2020-09-30 09:00:13 -0400

    Added ladders, climbing, cinemachine follow camera and confiner

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     // Config
     [SerializeField] float runSpeed = 10f;
     [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
 
     // State
     bool isAlive = true;
@@ -15,17 +16,22 @@ public class Player : MonoBehaviour
     // Cached component references
     Rigidbody2D myRigidBody;
     Animator myAnimator;
+    Collider2D myCollider2D;
+    float gravityScaleAtStart;
 
     // Messages then methods
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
+        gravityScaleAtStart = myRigidBody.gravityScale;
     }
 
     void Update()
     {
         Run();
+        ClimbLadder();
         Jump();
         FlipSprite();
     }
@@ -36,12 +42,32 @@ public class Player : MonoBehaviour
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
 
-        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon; // this means greater than 0s
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon; // The closest we can get to zero without being zero
         myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    void ClimbLadder()
+    {
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            myAnimator.SetBool("Climbing", false);
+            myRigidBody.gravityScale = gravityScaleAtStart;
+            return; // This returns control back to unity, so the next line of code will be executed
+        }
+        
+        float controlThrow = CrossPlatformInputManager.GetAxis("Vertical"); // value is between -1 to +1
+        Vector2 climbVelocity = new Vector2(myRigidBody.velocity.x, controlThrow * climbSpeed);
+        myRigidBody.velocity = climbVelocity;
+        myRigidBody.gravityScale = 0f;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon; // The closest we can get to zero without being zero
+        myAnimator.SetBool("Climbing", playerHasVerticalSpeed);
     }
 
     void Jump()
     {
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; } // This returns control back to unity, so the next line of code will be executed
+
         if (CrossPlatformInputManager.GetButtonDown("Jump"))
         {
             Vector2 jumpVelocityToAdd = new Vector2(0, jumpSpeed);
