76ac7441f4ce253e9573eed1e62a5bcafaca0ad3
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2020-10-04 13:22:36 -0400

    Added coin pickup SFX, persistent score and lives, rising water, and completed all 3 levels

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip coinPickUpSFX;
+    [SerializeField] int pointsForCoinPickUp = 100;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject);
+        var playerBody = FindObjectOfType<Player>().GetComponent<CapsuleCollider2D>();
+
+        if (collision == playerBody)
+        {
+            FindObjectOfType<GameSession>().AddToScore(pointsForCoinPickUp);
+            AudioSource.PlayClipAtPoint(coinPickUpSFX, Camera.main.transform.position);
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int playerLives = 3;
+    [SerializeField] int score = 0;
+
+    [SerializeField] Text livesText;
+    [SerializeField] Text scoreText;
 
     void Awake()
     {
@@ -19,6 +24,18 @@ public class GameSession : MonoBehaviour
         {
             DontDestroyOnLoad(gameObject);
         }
+    }
+
+    void Start()
+    {
+        livesText.text = playerLives.ToString();
+        scoreText.text = score.ToString();
+    }
+
+    public void AddToScore (int pointsToAdd)
+    {
+        score += pointsToAdd;
+        scoreText.text = score.ToString();
     }
 
     public void ProcessPlayerDeath()
@@ -38,6 +55,7 @@ public class GameSession : MonoBehaviour
         playerLives--; // Takes the current amount and substracts 1
         var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+        livesText.text = playerLives.ToString();
     }
 
     void ResetGameSession()
